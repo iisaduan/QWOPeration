39dd2999612408a74f37427662f64cbf94dd62e8
Anna <asinger@hmc.edu>
2020-02-27 20:42:14 -0800

    docs(Shop): added comments to methods

--- TowerDefenseTutorial/Assets/Scripts/Shop.cs
+++ TowerDefenseTutorial/Assets/Scripts/Shop.cs
@@ -5,17 +5,30 @@ public class Shop : MonoBehaviour
 {
     BuildManager buildManager;
 
+    /* Start() - called when game starts
+     *
+     * instantiates buildManager
+     * 
+     */
     private void Start()
     {
         buildManager = BuildManager.instance;
     }
 
+    /* PurchaseStandardTurret() - makes standard turret the turret to buy
+     * 
+     */
     public void PurchaseStandardTurret()
     {
         Debug.Log("Standard Turret Purchased");
         buildManager.setTurretToBuild(buildManager.standardTurretPrefab);
     }
 
+    /* PurchaseAnotherTurret() - makes another turret the turret to buy
+     *
+     * TODO: this doesn't do anything yet - wait until there is another turret
+     * 
+     */
     public void PurchaseAnotherTurret()
     {
         Debug.Log("Another Turret Purchased");
