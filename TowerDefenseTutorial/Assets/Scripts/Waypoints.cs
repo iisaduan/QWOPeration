ce89a6ce8a1c8cdb56cdc8a7ae47effd2e8a3e36
Anna <asinger@hmc.edu>
2020-02-28 13:30:06 -0800

    docs(Waypoints): added comments to methods

--- TowerDefenseTutorial/Assets/Scripts/Waypoints.cs
+++ TowerDefenseTutorial/Assets/Scripts/Waypoints.cs
@@ -5,6 +5,11 @@ public class Waypoints : MonoBehaviour
 {
     public static Transform[] points;
 
+    /* Awake() - done right before game starts
+     *
+     * creates an array of all waypoints (used in Enemy)
+     * 
+     */
     private void Awake()
     {
         points = new Transform[transform.childCount];
