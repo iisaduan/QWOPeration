ab59d75973d57fd68b6ae459c28dd6939913313e
Anna <asinger@hmc.edu>
2020-02-27 19:58:32 -0800

    fix(Turret): updated comments

--- TowerDefenseTutorial/Assets/Scripts/Turret.cs
+++ TowerDefenseTutorial/Assets/Scripts/Turret.cs
@@ -9,6 +9,7 @@ public class Turret : MonoBehaviour
 
     public float range = 15f;
     public float fireRate = 1f;
+    public float damage = 1f;
     private float fireCountdown = 0f;
 
     [Header("Unity Setup  Fields")]
@@ -21,11 +22,23 @@ public class Turret : MonoBehaviour
     public Transform firePoint;
     
 
+    /* Start()
+     *
+     * makes  target update twice every second
+     *
+     */
     void Start()
     {
         InvokeRepeating("UpdateTarget", 0f, .5f);
     }
 
+    /* UpdateTarget()
+     *
+     * updates turret's target by finding the closest enemy in range
+     *
+     * TODO: look at this for changing which enemy the turret shoots at
+     *
+     */
     void UpdateTarget()
     {
         GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
@@ -33,6 +46,7 @@ public class Turret : MonoBehaviour
         float shortestDistance = Mathf.Infinity;
         GameObject nearestEnemy = null;
 
+        // find closest enemy - MODIFY THIS IF WANT TO CHANGE SHOOTING BEHAVIOR
         foreach (GameObject enemy in enemies)
         {
             float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
@@ -44,6 +58,7 @@ public class Turret : MonoBehaviour
 
         }
 
+        // if  there is an enemy in range - set that to target
         if (nearestEnemy != null && shortestDistance <= range)
         {
             target = nearestEnemy.transform;
@@ -54,18 +69,27 @@ public class Turret : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+    /* Update() -  called once per frame
+     *
+     * makes turret rotate towards target
+     *
+     * determines when to shoot
+     *
+     */
     void Update()
     {
+        // if  there is no target - do nothing
         if (target == null)
         {
             return;
         }
+        // weird vector math to make the turret rotate towards the enemy
         Vector3 dir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime*turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
+        // determines if/wehn to shoot
         if(fireCountdown <= 0f)
         {
             Shoot();
@@ -76,6 +100,11 @@ public class Turret : MonoBehaviour
 
     }
 
+    /* Shoot()
+     *
+     * shoots a bullet towards target
+     *
+     */
     void Shoot()
     {
         GameObject bulletGO = (GameObject) Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
@@ -86,6 +115,14 @@ public class Turret : MonoBehaviour
         }
     }
 
+    /* OnDrawGizmosSelected()
+     *
+     * shows range of turret using a red sphere
+     * Only works in "create" mode 
+     *
+     * TODO: make this work in play mode
+     *
+     */
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
