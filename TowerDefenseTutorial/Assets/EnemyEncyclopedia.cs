b23d4e4c05d3cf635bb6b316709a7fb1e30b2195
Anna <asinger@hmc.edu>
2020-04-17 14:58:58 -0700

    feat(Encyclopedia): created scenes for color enemies

---
+++ TowerDefenseTutorial/Assets/EnemyEncyclopedia.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyEncyclopedia : MonoBehaviour
+{
+    public GameObject g;
+    Enemy e;
+    public Text speed;
+    public Text health;
+    public Text money;
+    public Text spawnType;
+
+    void Start()
+    {
+        e = g.GetComponent<Enemy>();
+        speed.text = "Speed: " + e.startSpeed;
+        health.text = "Health: " + e.startHealth;
+        money.text = "Money Gained: " + e.moneyGain;
+        
+        
+    }
+}
